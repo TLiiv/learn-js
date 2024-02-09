25250e4f722f015967c62a4802340cf259590400
Tarmo <liivamagi.tarmo@gmail.com>
2024-02-09 10:20:34 +0200

    multitple html-s in webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
     },
-    compress: true,
-    port: 9000,
-  },
-  plugins: [new HtmlWebpackPlugin()],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template:'./src/index.html',
+                filename:'./about.html'
+            }
+        )
+    ],
 };
